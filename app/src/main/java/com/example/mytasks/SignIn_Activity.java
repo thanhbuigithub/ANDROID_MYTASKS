63170db9e7dd5ea630b3e24e020c89588713bf2e
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2019-11-12 16:09:47 +0700

    Update signin_signup activity and layout . Save database in sqlite.

--- app/src/main/java/com/example/mytasks/SignIn_Activity.java
+++ app/src/main/java/com/example/mytasks/SignIn_Activity.java
@@ -7,18 +7,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mytasks.DatabaseHelper;
+import com.example.mytasks.R;
+import com.example.mytasks.SignUp_Activity;
 
 public class SignIn_Activity extends AppCompatActivity {
     TextView tvIntro_01,tvIntro_02,tvIntro_03,tvRegister;
     ImageView imgView;
     EditText edName,edPass;
     Button btnLogin;
+    DatabaseHelper db;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
+
+        db = new DatabaseHelper(this);
+
         tvIntro_01=(TextView)findViewById(R.id.text_welcome1);
         tvIntro_02=(TextView)findViewById(R.id.text_welcome2);
         tvIntro_03=(TextView)findViewById(R.id.text_welcome3);
@@ -26,24 +36,41 @@ public class SignIn_Activity extends AppCompatActivity {
         imgView=(ImageView)findViewById(R.id.image_logo);
         edName = (EditText)findViewById(R.id.edit_username);
         edPass = (EditText)findViewById(R.id.edit_password);
-        btnLogin = (Button)findViewById(R.id.btn_login);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mName=edName.getText().toString();
-                String mPass=edPass.getText().toString();
-                Intent intent=new Intent(SignIn_Activity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        btnLogin = (Button)findViewById(R.id.btn_signin);
 
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(SignIn_Activity.this,SignUp_Activity.class);
-                startActivity(intent);
+                Intent registerIntent =new Intent(SignIn_Activity.this, SignUp_Activity.class);
+                startActivity(registerIntent);
             }
         });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mName = edName.getText().toString();
+                String mPass = edPass.getText().toString();
+                Boolean mres = db.checkUser(mName, mPass);
+
+
+                if(mres == true)
+                {
+                    Toast.makeText(SignIn_Activity.this,"Đăng nhập thành công!",Toast.LENGTH_SHORT).show();
+                    Intent GoHomePage = new Intent(SignIn_Activity.this,MainActivity.class);
+                    startActivity(GoHomePage);
+                }
+                else if(mName.isEmpty()||mPass.isEmpty()){
+                    Toast.makeText(SignIn_Activity.this,"Tên đăng nhập hoặc mật khẩu không được bỏ trống",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(SignIn_Activity.this,"Tên đăng nhập hoặc mật khẩu không đúng",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
+
 }
